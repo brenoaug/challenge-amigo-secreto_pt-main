daf9f28682d8b528cd7c0379bf2200504bc7a722
breno augusto <breaugustocp@outlook.com>
2025-09-17 20:16:32 -0300

    feat(core): criação de uma lista e a função para adicionar nomes à lista

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let inputAmigo = document.getElementById("amigo");
+  let nomeAmigo = inputAmigo.value;
+  amigos.push(nomeAmigo);
+
+  inputAmigo.value = "";
+  
+  console.log(amigos);
+}
